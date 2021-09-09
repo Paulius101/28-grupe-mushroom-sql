501f38da942e4b9fc542765d75164b9f9711fa8b
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-09-09 09:24:39 +0300

    tasks completed

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.init = async () => {
             LEFT JOIN`mushroom` \
                 ON`mushroom`.`id` = `basket`.`mushroom_id` \
             GROUP BY`gatherer`.`name` \
-            ORDER BY`gatherer`.`name` ASC; ';
+            ORDER BY`mushroom`.`price` ASC; ';
     [rows] = await connection.execute(sql);
 
     console.log('Grybu krepselio kainos pas grybautoja:');
@@ -107,7 +107,86 @@ app.init = async () => {
     for (const { name, payment } of rows) {
         console.log(`${++i}) ${name} - ${(+payment).toFixed(2)} EUR`);
     }
+
+    // 8
+    console.log(``);
+    async function mushroomByRating(lang) {
+
+        const langList = ['en', 'lt', 'esp', 'lv'];
+
+        lang = langList.includes(lang) ? lang : langList[0];
+
+        sql = 'SELECT `ratings`.`id`, `name_' + lang + '`, SUM(`count`) as amount\
+        FROM `ratings`\
+        LEFT JOIN `mushroom`\
+            ON `mushroom`.`rating`=`ratings`.`id`\
+        LEFT JOIN `basket`\
+            ON `basket`.`mushroom_id` =`mushroom`.`id`\
+        GROUP BY `ratings`.`id`\
+        ORDER BY `ratings`.`id` DESC';
+
+        [rows] = await connection.execute(sql);
+
+        if (lang === 'lt') {
+
+            console.log(`Grybu kiekis pagal ivertinima:`);
+            for (let { id, name_lt, amount } of rows) {
+                if (amount == null) {
+                    amount = 0;
+                }
+                console.log(`${id} zvaigzdutes (${name_lt}) - ${amount} grybai`);
+            }
+        } else {
+            console.log('');
+            console.log(`Mushrooms count by rating:`);
+            for (let { id, name_en, amount } of rows) {
+                if (amount == null) {
+                    amount = 0;
+                }
+                console.log(`${id} stars (${name_en}) - ${amount} grybai`);
+            }
+        }
+    }
+    await mushroomByRating('lt');
+    console.log(``);
+    await mushroomByRating('en');
+
+
+    //**9** _Isspausdinti, visus grybus, kuriu ivertinimas geresnis arba lygus 4 zvaigzdutem, isrikiuotus gerejimo tvarka_
+
+    sql = 'SELECT `mushroom` as name, `rating`\
+    FROM `mushroom`\
+    WHERE `rating` >= 4\
+    ORDER BY `rating` ASC' ;
+
+    [rows] = await connection.execute(sql);
+
+    let mushroomList = [];
+
+    for (let { name } of rows) {
+        mushroomList.push(upName(name))
+    }
+    console.log('');
+    console.log(`Grybai: ${mushroomList.join(', ')}.`);
+
+    //**10** _Isspausdinti, visus grybus, kuriu ivertinimas yra viena is nurodytu reiksmiu: 1, 3 arba 5 zvaigzdutem, isrikiuotus gerejimo tvarka_
+
+    sql = 'SELECT `mushroom` as name, `rating`\
+    FROM `mushroom`\
+    WHERE rating IN (1,3,5) \
+    ORDER BY `rating` ASC' ;
+
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+    mushroomList = [];
+
+    for (let { name } of rows) {
+        mushroomList.push(upName(name))
+    }
+    console.log('');
+    console.log(`Grybai: ${mushroomList.join(', ')}.`);
 }
+
 
 app.init();
 
